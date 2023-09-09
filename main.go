1737a6d7570f37a8d283f5bfcb8324ac4af6f3e4
Phu Han <phuhanld@gmail.com>
2023-09-09 23:18:03 +0700

    feat: auto redirect to target server base on request

--- main.go
+++ main.go
@@ -20,27 +20,24 @@ type servicesServer struct {
 }
 
 func newServer(addr string) *servicesServer {
-	serverUrl, err := url.Parse(addr)
-
+	serverURL, err := url.Parse(addr)
 	handleError(err)
 
 	return &servicesServer{
 		addr:  addr,
-		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+		proxy: httputil.NewSingleHostReverseProxy(serverURL),
 	}
 }
 
 type LoadBalancer struct {
-	port            string
-	roundRobinCount int
-	servers         []Server
+	port    string
+	servers map[string]Server // Use a map for easy path-to-server mapping
 }
 
-func NewLoadBalancer(port string, servers []Server) *LoadBalancer {
+func NewLoadBalancer(port string, servers map[string]Server) *LoadBalancer {
 	return &LoadBalancer{
-		port:            port,
-		roundRobinCount: 0,
-		servers:         servers,
+		port:    port,
+		servers: servers,
 	}
 }
 
@@ -63,30 +60,21 @@ func (s *servicesServer) Serve(rw http.ResponseWriter, r *http.Request) {
 	s.proxy.ServeHTTP(rw, r)
 }
 
-func (lb *LoadBalancer) getNextAvailableServer() Server {
-	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
-	for !server.IsAlive() {
-		lb.roundRobinCount++
-		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
-	}
-	lb.roundRobinCount++
-	return server
-}
-
 func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {
-	targetServer := lb.getNextAvailableServer()
-	fmt.Printf("forwarding request to address %q\n", targetServer.Address())
-	targetServer.Serve(rw, r)
+	path := r.URL.Path
+	if server, ok := lb.servers[path]; ok {
+		fmt.Printf("forwarding request to address %q\n", server.Address())
+		server.Serve(rw, r)
+	} else {
+		http.Error(rw, "Not Found", http.StatusNotFound)
+	}
 }
 
 func main() {
-	servers := []Server{
-		// Trip service
-		newServer("localhost:3081"),
-		// Notification SSE service
-		newServer("localhost:3082"),
-		// Geolocation service
-		newServer("localhost:3083"),
+	servers := map[string]Server{
+		"/trip":         newServer("http://localhost:3081"), // Trip service
+		"/notification": newServer("http://localhost:3082"), // Notification service
+		"/geolocation":  newServer("http://localhost:3083"), // Geolocation service
 	}
 	lb := NewLoadBalancer("9090", servers)
 
@@ -95,6 +83,6 @@ func main() {
 	}
 	http.HandleFunc("/", handleRedirect)
 
-	fmt.Printf("Serving requests at 'localhost: %s'\n", lb.port)
+	fmt.Printf("Serving requests at 'localhost:%s'\n", lb.port)
 	http.ListenAndServe(":"+lb.port, nil)
 }
